b26d1911aa7cd7110fb5f56708444b7cdbb990ec
Anindit Gopalakrishnan <aninditgo@Anindits-MacBook-Pro.local>
2020-01-27 14:26:02 -0800

    praying to the lord

--- OfficeHours.py
+++ OfficeHours.py
@@ -5,6 +5,7 @@ from flask_heroku import Heroku
 import csv
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/aninditgo'
 #app.config['SQLALCHEMY_ECHO'] = True
 app.permanent_session_lifetime = datetime.timedelta(days=365)
@@ -433,5 +434,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(12)
     app.run()
