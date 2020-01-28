871cc505eddff7a8365fde676c8d9771fc6fffd2
Anindit Gopalakrishnan <aninditgo@Anindits-MacBook-Pro.local>
2020-01-27 17:03:11 -0800

    test appears to have failed

--- OfficeHours.py
+++ OfficeHours.py
@@ -9,7 +9,7 @@ app.secret_key = os.urandom(12)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/aninditgo'
 app.config['SQLALCHEMY_ECHO'] = True
 app.permanent_session_lifetime = datetime.timedelta(days=365)
-#heroku = Heroku(app)
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 
